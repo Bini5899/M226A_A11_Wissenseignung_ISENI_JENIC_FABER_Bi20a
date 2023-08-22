d1c060ac07c0ca2f95a4677f2632c9b30e155af4
Faber Lorin, GHR-POD-NEX-35 <lorin.faber@swisscom.com>
2023-08-22 16:55:45 +0200

    Started with book tasks 1.4

---
+++ WarmUP_M226_Lorin/src/Arthur.java
@@ -0,0 +1,10 @@
+public class Arthur {
+    public static void main(String[] args) {
+        System.out.println("Hallo Leute, ich bin Arthur, euer Produzent");
+
+        // Initializing a new instance of "Regisseur" with the name "david"
+        Regisseur david = new Regisseur();
+        david.go();
+        david.stop();
+    }
+}
---
+++ WarmUP_M226_Lorin/src/Regisseur.java
@@ -0,0 +1,9 @@
+public class Regisseur {
+    public void go() {
+        System.out.println("Klappe, die erste Action...");
+    }
+
+    public void stop() {
+        System.out.println("Alles im Kasten. Szene gestorben.");
+    }
+}
