9450ff3c1596bc68633d3981eacbf2ee7fdafa3c
Bini5899 <harbiniseni63@gmail.com>
2023-08-22 15:06:12 +0200

    Aufgabe 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,10 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
-        System.out.println("Bitte geben Sie mir einen Input:");
-        String input = myScanner.nextLine();  // Read user input
-        System.out.println("Sie haben: " + input + " eingegeben.");
+
+        Aufgabe1 aufgabe = new Aufgabe1();
+        aufgabe.Aufgabe1();
+
     }
 }
